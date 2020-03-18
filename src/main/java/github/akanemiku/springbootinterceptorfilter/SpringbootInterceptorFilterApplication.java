2c52a997778d5dc56e93f2d7e0e474bec2871359
Akanemiku <806394913@qq.com>
2020-03-18 14:08:49 +0800

    -init-

---
+++ src/main/java/github/akanemiku/springbootinterceptorfilter/SpringbootInterceptorFilterApplication.java
@@ -0,0 +1,13 @@
+package github.akanemiku.springbootinterceptorfilter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootInterceptorFilterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootInterceptorFilterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/github/akanemiku/springbootinterceptorfilter/SpringbootInterceptorFilterApplicationTests.java
@@ -0,0 +1,13 @@
+package github.akanemiku.springbootinterceptorfilter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootInterceptorFilterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
